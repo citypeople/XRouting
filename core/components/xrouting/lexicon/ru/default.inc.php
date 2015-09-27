b8ec5eb621916e80a2144d158f17d1d7585ebab0
damapic <butakov@damapic.ru>
2015-09-27 12:29:10 +0500

    Addition of Russian language

---
+++ core/components/xrouting/lexicon/ru/default.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Default English lexicon
+ */
+
+// System settings
+$_lang['setting_xrouting.include_www'] = 'Использовать WWW-поддомен';
+$_lang['setting_xrouting.include_www_desc'] = 'Определяет WWW-поддомен как основной контекст, т.е. www.example.com будет возвращать example.com.';
+$_lang['setting_xrouting.default_context'] = 'Контекст по умолчанию';
+$_lang['setting_xrouting.default_context_desc'] = 'Контекст по умолчанию, если параметр не задан xrouting.show_no_match_error установите значение НЕТ';
+$_lang['setting_xrouting.show_no_match_error'] = 'Отображать ошибки';
+$_lang['setting_xrouting.show_no_match_error_desc'] = 'Если включить, XRouting вернет ошибку вместо контектста по умолчанию.';
+$_lang['setting_xrouting.allow_debug_info'] = 'Включить отладку';
+$_lang['setting_xrouting.allow_debug_info_desc'] = 'Для получения информации используйте GET параметр &xrouting-debug=1 (добавить в адресную строку). ВАЖНО: не забудьте отключить отладку после завершения работ, т.к. это подвергает ваш сайт опасности!';
